87c3a27c0693772c18cfca1418663b9675c79f1a
Stanislav Bondarenko <stassss2009@gmail.com>
2021-05-21 05:25:00 +0300

    feat(client): add client-side sockets realization, add Newtonsoft lib for de/serialization

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+using Server;
 
 namespace Client
 {
     class Program
     {
+        private const int Port = 12000;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+
+            var query = "";
+            while (query != "\\q")
+            {
+                var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipPoint);
+                Console.Write("Input query:");
+                query = Console.ReadLine();
+                var reqObject = new Request
+                {
+                    Operation = "get",
+                    Query = query
+                };
+                var data = Encoding.Unicode.GetBytes(JsonConvert.SerializeObject(reqObject));
+                socket.Send(data);
+
+                data = new byte[256];
+                var builder = new StringBuilder();
+
+                do
+                {
+                    builder.Append(Encoding.Unicode.GetString(data, 0,
+                        socket.Receive(data, data.Length, 0)));
+                } while (socket.Available > 0);
+
+                var resp = JsonConvert.DeserializeObject<Response>(builder.ToString());
+                Console.WriteLine($"Response:\n\tQuery - {resp.Query}\n\tCount - {resp.Count}");
+                Console.WriteLine(String.Join("\n", resp.Instances));
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
         }
     }
 }
