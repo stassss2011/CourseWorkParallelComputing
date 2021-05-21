5eaa21fefa99b40f2e9ac5cd77dfa1b51d2305c1
Stanislav Bondarenko <stassss2009@gmail.com>
2021-05-21 05:24:43 +0300

    feat(server): add server-side sockets realization, add Newtonsoft lib for de/serialization

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -9,6 +9,7 @@ namespace Server
 {
     class Program
     {
+        private const int Port = 12000;
 
         static void Main(string[] args)
         {
@@ -19,6 +20,50 @@ namespace Server
             var index = indexBuilder.UpdateFromPath(dir);
             stopwatch.Stop();
             Console.WriteLine($"Index build in {stopwatch.Elapsed}s");
+
+            var ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), Port);
+
+            var listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            listenSocket.Bind(ipPoint);
+            listenSocket.Listen();
+
+            Console.WriteLine("Server started. Waiting for connections...");
+
+            while (true)
+            {
+                var handler = listenSocket.Accept();
+                var builder = new StringBuilder();
+                var data = new byte[256];
+                do
+                {
+                    builder.Append(Encoding.Unicode.GetString(data, 0, handler.Receive(data)));
+                } while (handler.Available > 0);
+
+                Console.WriteLine(builder.ToString());
+
+                var req = JsonConvert.DeserializeObject<Request>(builder.ToString())
+                          ?? throw new Exception("Serialization error");
+
+                string resp;
+                if (req.Operation == "get")
+                {
+                    var respObject = new Response()
+                    {
+                        Query = req.Query,
+                        Instances = index.Get(req.Query)
+                    };
+                    resp = JsonConvert.SerializeObject(respObject);
+                }
+                else
+                {
+                    continue;
+                }
+
+                data = Encoding.Unicode.GetBytes(resp);
+                handler.Send(data);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
         }
     }
 }
